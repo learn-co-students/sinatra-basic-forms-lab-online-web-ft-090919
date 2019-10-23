660e8a6cf91602ee9574d36d554d1e329cfbe42e
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-10-23 15:11:25 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,12 +1,14 @@
 
-class Puppy < Sinatra::Base
+class Puppy 
     attr_accessor :name, :breed, :months_old
 
     def initialize(name, breed, months_old)
         @name = name
         @breed = breed
         @months_old = months_old
+
     end
+ 
 
 
 end
